89e9e8d933c8fd4e4d8e27a4e219b0569fba179f
George Corney <haxiomic@gmail.com>
2018-12-08 20:00:04 +0000

    Update build

--- dist/GPUText.js
+++ dist/GPUText.js
@@ -166,14 +166,15 @@ var GPUText = /** @class */ (function () {
         var dataView = new DataView(buffer);
         // read header string, expect utf-8 encoded
         // the end of the header string is marked by a null character
-        var jsonHeader = '';
         var p = 0;
+        // end of header is marked by first 0x00 byte
         for (; p < buffer.byteLength; p++) {
             var byte = dataView.getInt8(p);
             if (byte === 0)
                 break;
-            jsonHeader += String.fromCharCode(byte);
         }
+        var headerBytes = new Uint8Array(buffer, 0, p);
+        var jsonHeader = decodeUTF8(headerBytes);
         // payload is starts from the first byte after the null character
         var payloadStart = p + 1;
         var littleEndian = true;
@@ -279,6 +280,43 @@ var GPUText = /** @class */ (function () {
     };
     return GPUText;
 }());
+// credits github user pascaldekloe
+// https://gist.github.com/pascaldekloe/62546103a1576803dade9269ccf76330
+function decodeUTF8(bytes) {
+    var i = 0, s = '';
+    while (i < bytes.length) {
+        var c = bytes[i++];
+        if (c > 127) {
+            if (c > 191 && c < 224) {
+                if (i >= bytes.length)
+                    throw new Error('UTF-8 decode: incomplete 2-byte sequence');
+                c = (c & 31) << 6 | bytes[i++] & 63;
+            }
+            else if (c > 223 && c < 240) {
+                if (i + 1 >= bytes.length)
+                    throw new Error('UTF-8 decode: incomplete 3-byte sequence');
+                c = (c & 15) << 12 | (bytes[i++] & 63) << 6 | bytes[i++] & 63;
+            }
+            else if (c > 239 && c < 248) {
+                if (i + 2 >= bytes.length)
+                    throw new Error('UTF-8 decode: incomplete 4-byte sequence');
+                c = (c & 7) << 18 | (bytes[i++] & 63) << 12 | (bytes[i++] & 63) << 6 | bytes[i++] & 63;
+            }
+            else
+                throw new Error('UTF-8 decode: unknown multibyte start 0x' + c.toString(16) + ' at index ' + (i - 1));
+        }
+        if (c <= 0xffff)
+            s += String.fromCharCode(c);
+        else if (c <= 0x10ffff) {
+            c -= 0x10000;
+            s += String.fromCharCode(c >> 10 | 0xd800);
+            s += String.fromCharCode(c & 0x3FF | 0xdc00);
+        }
+        else
+            throw new Error('UTF-8 decode: code point 0x' + c.toString(16) + ' exceeds UTF-16 reach');
+    }
+    return s;
+}
 exports.default = GPUText;
 if (typeof window !== 'undefined') {
     // expose GPUText on the window object
