e947ff798bf6094f1eeb72d9393777eebe5dbf70
George Corney <haxiomic@gmail.com>
2019-02-18 20:30:53 +0000

    Add copy-to-examples dir command

--- generate/example.sh
+++ generate/example.sh
@@ -1,1 +1,2 @@
-node generate.js source-fonts/OpenSans/OpenSans-Regular.ttf --binary true --charset charsets/example.txt
+node generate.js source-fonts/OpenSans/OpenSans-Regular.ttf --binary true --charset charsets/example.txt &&
+cp OpenSans-Regular* ../example/
